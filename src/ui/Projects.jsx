89b7b97944ca08314df2f63a590d981204f28d20
yuan0-0jia <yjia0125@outlook.com>
2024-04-25 15:59:32 -0700

    Update Project Sorting

--- src/ui/Projects.jsx
+++ src/ui/Projects.jsx
@@ -6,13 +6,13 @@ function Projects() {
   const { isLoading, projects } = useProjects();
 
   return (
-    <section className="mt-40 px-4 py-4 bg-[#f7f7f7] tracking-wide">
+    <section className="mt-40 bg-[#f7f7f7] px-4 py-4 tracking-wide">
       <div className="text-center">
         <header>
-          <h2 className="before:mx-auto before:my-8 before:block before:w-16 before:h-1px before:border-t before:border-slate-500 before:opacity-35 text-4xl font-extralight tracking-wide">
+          <h2 className="before:h-1px text-4xl font-extralight tracking-wide before:mx-auto before:my-8 before:block before:w-16 before:border-t before:border-slate-500 before:opacity-35">
             Projects
           </h2>
-          <p className=" p-2 text-stone-500 font-extralight text-md">
+          <p className=" text-md p-2 font-extralight text-stone-500">
             Here are some projects I did over the years.
           </p>
         </header>
@@ -20,20 +20,22 @@ function Projects() {
 
       {isLoading ? (
         <div className="my-20 text-center">
-          <Spinner className="fill-zinc-600 w-28 h-28" />
+          <Spinner className="h-28 w-28 fill-zinc-600" />
         </div>
       ) : (
-        projects?.map((project) => (
-          <Project
-            key={project.id}
-            header={project.project}
-            desc={project.desc}
-            image={project.thumbnail}
-            reverse={project.id % 2 === 0}
-            to={project.to}
-            button={project.button}
-          />
-        ))
+        projects
+          ?.sort((cur, next) => cur.id - next.id)
+          .map((project) => (
+            <Project
+              key={project.id}
+              header={project.project}
+              desc={project.desc}
+              image={project.thumbnail}
+              reverse={project.id % 2 === 0}
+              to={project.to}
+              button={project.button}
+            />
+          ))
       )}
     </section>
   );
